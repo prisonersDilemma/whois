ce789766293a32b8b14328dd63e18f89cc445d38
GH05T <outoftheshadowsintotheabyss@gmail.com>
2017-12-01 20:29:55 -0800

    Fully operational. A few minor fixes between __main__ and argparser.

--- whois/__init__.py
+++ whois/__init__.py
@@ -4,11 +4,5 @@ from . import database
 from . import nacat
 from . import targets
 
-# Not sure if this will work if the packages aren't all installed separately.
-from py3mods import bytepos
-from py3mods import printables
-from py3mods import tail
-from py3mods import yyyymmdd
-
 global PKGPATH # Not even sure how global works.
 PKGPATH = __path__[0]
