a3f7201c71812c3a8a745557aa05d2847bbd1bda
GH05T <outoftheshadowsintotheabyss@gmail.com>
2017-12-01 21:51:17 -0500

    Pushing because files were missing, such as setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3.6
+
+from setuptools import setup
+
+setup(name='whois',
+      description='Write the results of whois queries generated from a log to a database.',
+      version='0.2',
+      author='na',
+      license='Apache',
+      zipsafe=False,
+      )
+
+      #packages=['bytepos', 'tables', 'tail', 'yyyymmdd'],
+
+if __name__ == '__main__':
+    pass
